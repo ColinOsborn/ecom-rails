714eedc7720b3fb0bd56f9e616a8bee3f6d1fd1d
Colin Osborn <colin.osborn@gmail.com>
2024-10-23 15:03:35 -0600

    implemented basic admin controller model and view with basic test

---
+++ spec/controllers/admin_controller_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe AdminController, type: :controller do
+  describe 'GET index' do
+    it 'renders the admin index template' do
+      get :index
+      expect(response).to render_template('index')
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe HomeController, type: :controller do
     it 'renders the index template' do
       get :index
       expect(response).to render_template('index')
+      expect(response).to have_http_status(200)
     end
   end
 end
