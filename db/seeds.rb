a0449441865bf74f9090af0510ee4b665772c394
Colin Osborn <colin.osborn@gmail.com>
2024-11-11 18:07:43 -0700

    implemented category seeds for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# Categories creation
+
+(1..10).each do |i|
+  Category.create!(name: "Category #{i}", description: "unc a ullamcorper lectus. Vivamus ullamcorper rutrum pulvinar." +
+  "Donec maximus neque vitae tristique rutrum.")
+end
