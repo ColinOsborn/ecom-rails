e7878098370c59d9a2e55d965708e7939e28213a
Colin Osborn <colin.osborn@gmail.com>
2025-02-07 14:52:35 -0700

    set params around image sizing and relationship

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,9 @@ class Category < ApplicationRecord
   # Validations
   validates :name, presence: true
   validates :description, presence: true
+
+  # active_storage images
+  has_one_attached :image do |attachable|
+    attachable.variant :thumb, resize_to_limit: [50,50]
+  end
 end
